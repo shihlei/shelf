47423e2de39ffaf8bc6546525e4fe5bdd0f9fd82
Shih-Lei (Laura) Chen <70946401+shihlei@users.noreply.github.com>
2025-09-06 12:05:18 -0700

    Create solution.js

---
+++ Leetcode/JS30/2667. Create Hello World Function/solution.js
@@ -0,0 +1,9 @@
+/**
+ * @return {Function}
+ */
+ //create a variavle and assign with a function
+const createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
