bb0ebd2c1a6a91604a50f87ee96e2af81a7a4f48
Shih-Lei (Laura) Chen <70946401+shihlei@users.noreply.github.com>
2025-09-07 22:39:34 -0700

    Create solution.js

---
+++ Leetcode/JS30/2665. Counter II/solution.js
@@ -0,0 +1,69 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+// Approach 1: Closure
+// Outer function is like a constructor
+var createCounter = function(init) {
+    // init and count are the hidden state that can be access
+    let count = init;
+
+    function increment(){
+        return ++count;
+    }
+
+    const reset = () => {
+        count = init;
+        return count;
+    }
+
+    const decrement = () => {
+        return --count;
+    }
+
+    // return object(mappings from strings to other value) to expose values
+    return{
+        increment:increment,
+        reset:reset,
+        decrement:decrement
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+
+
+
+//Approach 2: Class
+class createCounter{
+    constructor(init){
+        this.init = init; // init will out of scope
+        this.count= init;
+    }
+
+    // Don't need "function" keyword for function in class
+    increment(){
+        return ++this.count;
+    }
+
+    decrement = () => {
+        return --this.count;
+    }
+
+    reset = () => {
+        this.count = this.init;
+        return this.count;
+    }
+}
+
+/**
+ * const counter = new createCounter(5);
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
